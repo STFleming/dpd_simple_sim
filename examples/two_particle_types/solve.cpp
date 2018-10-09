ae3f30257f2d9e2a6e43322167bcfaab56d68226
STFleming <sf306@ic.ac.uk>
2018-10-09 20:43:33 +0100

    fixing the bugs in the 3D test

--- examples/two_particle_types/solve.cpp
+++ examples/two_particle_types/solve.cpp
@@ -5,12 +5,11 @@
 #include "utils.hpp"
 #include <random>
 
-#define DELTA_T 0.0001 
+#define DELTA_T 0.01 
 #define UNISIZE_D 10.0 // the size of a single dimension of the universe
 #define R_C 1.0 
 
-const float A[2][2] = { {-1.0, 2.0}, {2.0, -1.0}}; // interaction matrix
-//const float A[2][2] = { {0.0, 0.0}, {0.0, 0.0}}; // interaction matrix
+const float A[2][2] = { {-0.25, 4.0}, {4.0, 6.0}}; // interaction matrix
 
 // conservative pairwise force declaration
 void conF(Particle *me, Particle *other){
@@ -21,74 +20,79 @@ void conF(Particle *me, Particle *other){
     Vector3D r_i = me->getPos();
     Vector3D r_j = other->getPos();
     float r_ij_dist = r_i.toroidal_dist(r_j, UNISIZE_D); // get the distance
-    Vector3D r_ij = r_j - r_i; // vector between the points
- 
+    //Vector3D r_ij = r_i - r_j; // vector between the points
+    Vector3D r_ij = r_i.toroidal_subtraction(r_j, UNISIZE_D, R_C);
+
     // Equation 8.5 in the dl_meso manual
-    Vector3D force = (r_ij/r_ij_dist) * a_ij * (1.0 - (r_ij_dist/r_c));
+    Vector3D force = (r_ij/r_ij_dist) * (a_ij * (1.0 - (r_ij_dist/r_c)));
+
+    assert(r_ij_dist <= R_C);
 
     // update the forces acting on the two particles
-    me->setForce( me->getForce() + force); 
-    other->setForce( other->getForce() + force*-1.0); 
+    me->setForce( me->getForce() + force);  
+    other->setForce( other->getForce() + force*-1.0);  
     return;
 }
 
+
 // drag pairwie force declaration
 void dragF(Particle *me, Particle *other) {
-    
-    const float r_c = R_C; // the interaction cutoff
-    const float drag_coef = 0.3; // the drag coefficient (no idea what to set this at)
 
-    // position and distance
+    const float r_c = R_C; // the interaction cutoff
+    const float drag_coef = 0.998; // the drag coefficient (no idea what to set this at)
+
+   // // position and distance
     Vector3D r_i = me->getPos();
     Vector3D r_j = other->getPos();
     float r_ij_dist = r_i.toroidal_dist(r_j, UNISIZE_D); // get the distance
-    Vector3D r_ij = r_j - r_i; // vector between the points
+    //Vector3D r_ij = r_j - r_i; // vector between the points
+    Vector3D r_ij = r_i.toroidal_subtraction(r_j, UNISIZE_D, R_C);
 
     // switching function
-    float w_d = (1.0 - r_ij_dist / r_c)*(1.0 - r_ij_dist / r_c)*(1.0 - r_ij_dist / r_c)*(1.0 - r_ij_dist / r_c);
+    float w_d = (1.0 - r_ij_dist / r_c)*(1.0 - r_ij_dist / r_c);
 
     // velocities
-    Vector3D v_i = me->getVelo(); 
+    Vector3D v_i = me->getVelo();
     Vector3D v_j = other->getVelo();
     Vector3D v_ij = v_i - v_j; // relative velocity
-     
-    // Equation 8.7 in dl_meso manual
-    Vector3D force = (r_ij / (r_ij_dist * r_ij_dist)) * w_d * r_ij.dot(v_ij) * (-1.0 * drag_coef); 
+
+    Vector3D force = (r_ij / (r_ij_dist * r_ij_dist)) * w_d * r_ij.dot(v_ij) * (-1.0 * drag_coef);
 
     // update the forces acting on the two particles
-    me->setForce( me->getForce() + force); 
-    other->setForce( other->getForce() + force*-1.0); 
+    me->setForce( me->getForce() + force);
+    other->setForce( other->getForce() + force*-1.0);
     return;
 }
 
 // random pairwise force declaration
 void randF(Particle *me, Particle *other) {
 
-   float rand_var = (rand() / (float)RAND_MAX * 1.0);
-
-   const float K_BT = 500.0;
-   const float drag_coef = 0.3; // the drag coefficient (no idea what to set this at)
+   const float K_BT = 35.0;
+   const float drag_coef = 0.998; // the drag coefficient (no idea what to set this at)
    const float sigma_ij = sqrt(2*drag_coef*K_BT); // the temperature coef
-   const float dt = DELTA_T; 
+   const float dt = DELTA_T;
    const float r_c = R_C; // the interaction cutoff
 
    // position and distance
    Vector3D r_i = me->getPos();
    Vector3D r_j = other->getPos();
    float r_ij_dist = r_i.toroidal_dist(r_j, UNISIZE_D); // get the distance
-   Vector3D r_ij = r_j - r_i; // vector between the points
+   //Vector3D r_ij = r_j - r_i; // vector between the points
+   Vector3D r_ij = r_i.toroidal_subtraction(r_j, UNISIZE_D, R_C);
 
    // switching function
-   float w_r = (1.0 - r_ij_dist/r_c)*(1.0 - r_ij_dist/r_c);
-      
-   // Equation 8.6 in dl_meso for random force 
-   Vector3D force = (r_ij / r_ij_dist)*sqrt(dt)*rand_var*w_r*sigma_ij;  
+   float w_r = (1.0 - r_ij_dist/r_c);
+
+   // force calculation
+   float r = (rand() / (float)RAND_MAX * 1.0);
+   Vector3D force = (r_ij / r_ij_dist)*sqrt(dt)*r*w_r*sigma_ij;
 
    // update the forces acting on the two particles
-   me->setForce( me->getForce() + force); 
-   other->setForce( other->getForce() + force*-1.0); 
+   me->setForce( me->getForce() + force);
+   other->setForce( other->getForce() + force*-1.0);
    return;
 }
+
 
 // Test program
 int main() {
