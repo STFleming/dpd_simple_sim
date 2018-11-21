88466b907934b72618cfde887472a23281831a30
STFleming <sf306@ic.ac.uk>
2018-11-21 08:54:30 +0000

    Partial templated vesicle example -- only float

--- examples/vesicles/solve.cpp
+++ examples/vesicles/solve.cpp
@@ -4,31 +4,54 @@
 #include "Particle.hpp"
 #include "utils.hpp"
 #include <random>
+#include "fixed_ap.h"
 
 //#define DELTA_T 0.02 
 #define DELTA_T 0.01 
 #define UNISIZE_D 10.0 // the size of a single dimension of the universe
 #define R_C 1.0 
 
+typedef float p_type;
+//typedef fixap<int32_t, 27> p_type;
+
+Vector3D<p_type> randPos(unsigned N){
+    Vector3D<p_type> t_pos;
+    p_type x(rand() / (float)RAND_MAX * N);
+    p_type y(rand() / (float)RAND_MAX * N);
+    p_type z(rand() / (float)RAND_MAX * N);
+    t_pos.set(x,y,z);
+    return t_pos;
+}
+
+//! generates a random position within a given space (NxN)
+Vector3D<p_type> rand2DPos(unsigned N){
+    Vector3D<p_type> t_pos;
+    p_type x(rand() / (float)RAND_MAX * N);
+    p_type y(rand() / (float)RAND_MAX * N);
+    p_type z(0.0);
+    t_pos.set(x,y,z);
+    return t_pos;
+}
+
 // S = 0; B = 1; A = 2;
                         // S     B      A
-const float A[3][3] = { {15.0, 120.0, 15.0 },
-                        {120.0, 15.0, 120.0},
-                        {15.0, 120.0, 15.0 }}; // interaction matrix
+const p_type A[3][3] = { {p_type(15.0 ), p_type(120.0), p_type(15.0 )},
+                         {p_type(120.0), p_type(15.0 ), p_type(120.0)},
+                         {p_type(15.0 ), p_type(120.0), p_type(15.0 )}}; // interaction matrix
 
 // conservative pairwise force declaration
-void conF(Particle *me, Particle *other){
+void conF(Particle<p_type> *me, Particle<p_type> *other){
 
-    const float a_ij = A[me->getType()][other->getType()]; // the interaction strength
-    const float r_c = R_C; // the interaction cutoff
+    p_type a_ij = A[me->getType()][other->getType()]; // the interaction strength
+    const p_type r_c(R_C); // the interaction cutoff
 
-    Vector3D r_i = me->getPos();
-    Vector3D r_j = other->getPos();
-    float r_ij_dist = r_i.dist(r_j); // get the distance
-    Vector3D r_ij = r_i - r_j; // vector between the points
+    Vector3D<p_type> r_i = me->getPos();
+    Vector3D<p_type> r_j = other->getPos();
+    p_type r_ij_dist = r_i.dist(r_j); // get the distance
+    Vector3D<p_type> r_ij = r_i - r_j; // vector between the points
  
     // Equation 8.5 in the dl_meso manual
-    Vector3D force = (r_ij/r_ij_dist) * (a_ij * (1.0 - (r_ij_dist/r_c)));
+    Vector3D<p_type> force = (r_ij/r_ij_dist) * (a_ij * (p_type(1.0) - (r_ij_dist/r_c)));
 
     // update the forces acting on the two particles
     //me->setForce( me->getForce() + force); 
@@ -37,26 +60,26 @@ void conF(Particle *me, Particle *other){
 }
 
 // drag pairwie force declaration
-void dragF(Particle *me, Particle *other) {
+void dragF(Particle<p_type> *me, Particle<p_type> *other) {
     
-    const float r_c = R_C; // the interaction cutoff
-    const float drag_coef = 4.5; // the drag coefficient (no idea what to set this at)
+    const p_type r_c(R_C); // the interaction cutoff
+    const p_type drag_coef(4.5); // the drag coefficient (no idea what to set this at)
 
    // // position and distance
-    Vector3D r_i = me->getPos();
-    Vector3D r_j = other->getPos();
-    float r_ij_dist = r_i.dist(r_j); // get the distance
-    Vector3D r_ij = r_j - r_i; // vector between the points
+    Vector3D<p_type> r_i = me->getPos();
+    Vector3D<p_type> r_j = other->getPos();
+    p_type r_ij_dist = r_i.dist(r_j); // get the distance
+    Vector3D<p_type> r_ij = r_j - r_i; // vector between the points
 
     // switching function
-    float w_d = (1.0 - r_ij_dist / r_c)*(1.0 - r_ij_dist / r_c);
+    p_type w_d = (p_type(1.0) - r_ij_dist / r_c)*(p_type(1.0) - r_ij_dist / r_c);
 
     // velocities
-    Vector3D v_i = me->getVelo(); 
-    Vector3D v_j = other->getVelo();
-    Vector3D v_ij = v_i - v_j; // relative velocity
+    Vector3D<p_type> v_i = me->getVelo(); 
+    Vector3D<p_type> v_j = other->getVelo();
+    Vector3D<p_type> v_ij = v_i - v_j; // relative velocity
      
-    Vector3D force = (r_ij / (r_ij_dist * r_ij_dist)) * w_d * r_ij.dot(v_ij) * (-1.0 * drag_coef); 
+    Vector3D<p_type> force = (r_ij / (r_ij_dist * r_ij_dist)) * w_d * r_ij.dot(v_ij) * (p_type(-1.0) * drag_coef); 
 
     // update the forces acting on the two particles
     me->setForce( me->getForce() + force); 
@@ -75,62 +98,60 @@ uint32_t pairwise_rand(uint32_t grand, uint32_t pid1, uint32_t pid2){
 }
 
 // random pairwise force declaration
-void randF(uint32_t grand, Particle *me, Particle *other) {
+void randF(uint32_t grand, Particle<p_type> *me, Particle<p_type> *other) {
 
-   const float K_BT = 1.0;
-   const float drag_coef = 4.5; // the drag coefficient (no idea what to set this at)
-   const float sigma_ij = sqrt(2*drag_coef*K_BT); // the temperature coef
-   const float dt = DELTA_T;
-   const float r_c = R_C; // the interaction cutoff
+   const p_type K_BT(1.0);
+   const p_type drag_coef(4.5); // the drag coefficient (no idea what to set this at)
+   const p_type sigma_ij(sqrt(2*drag_coef*K_BT)); // the temperature coef
+   const p_type dt(DELTA_T);
+   const p_type r_c(R_C); // the interaction cutoff
 
    // position and distance
-   Vector3D r_i = me->getPos();
-   Vector3D r_j = other->getPos();
-   float r_ij_dist = r_i.dist(r_j); // get the distance
-   Vector3D r_ij = r_j - r_i; // vector between the points
+   Vector3D<p_type> r_i = me->getPos();
+   Vector3D<p_type> r_j = other->getPos();
+   p_type r_ij_dist = r_i.dist(r_j); // get the distance
+   Vector3D<p_type> r_ij = r_j - r_i; // vector between the points
 
    // switching function
-   float w_r = (1.0 - r_ij_dist/r_c);
+   p_type w_r = (p_type(1.0) - r_ij_dist/r_c);
 
    // random number generation
-   float r = ((pairwise_rand(grand, me->getID(), other->getID()) / (float)(RAND_MAX)) * 0.5);
+   p_type r((pairwise_rand(grand, me->getID(), other->getID()) / (float)(RAND_MAX)) * 0.5);
 
    // force calculation
-   Vector3D force = (r_ij / r_ij_dist)*sqrt(dt)*r*w_r*sigma_ij;
-
-   //printf("p:%d <-> p:%d  r=%u   f=%s\n", me->getID(), other->getID(), pairwise_rand(grand, me->getID(), other->getID()), force.str().c_str()); 
+   Vector3D<p_type> force = (r_ij / r_ij_dist)*sqrt(dt)*r*w_r*sigma_ij;
 
    // update the forces acting on the two particles
-   me->setForce( me->getForce() + force*-1.0);
+   me->setForce( me->getForce() + force*p_type(-1.0));
 }
 
 
 // this is a hookean harmonic bond 
-void bondF(Particle *me, Particle *other) {
+void bondF(Particle<p_type> *me, Particle<p_type> *other) {
 
-   const float K_div_2 = 160.0/2; // spring constant guess at a value 
-   const float r_o = 0.38; // the equilibrium bond length 
+   const p_type K_div_2(128.0/2); // spring constant guess at a value 
+   const p_type r_o(0.5); // the equilibrium bond length 
 
    // variables for the current position
-   Vector3D r_i = me->getPos();
-   Vector3D r_j = other->getPos();
-   float r_ij_dist = r_i.dist(r_j); // get the distance
-   Vector3D r_ij = r_i - r_j;
+   Vector3D<p_type> r_i = me->getPos();
+   Vector3D<p_type> r_j = other->getPos();
+   p_type r_ij_dist = r_i.dist(r_j); // get the distance
+   Vector3D<p_type> r_ij = r_i - r_j;
 
    // variables for the previous position
-   Vector3D p_r_i = me->getPrevPos();
-   Vector3D p_r_j = other->getPrevPos();
-   float p_r_ij_dist = p_r_i.dist(p_r_j);
-   Vector3D p_r_ij = p_r_i - p_r_j;
+   Vector3D<p_type> p_r_i = me->getPrevPos();
+   Vector3D<p_type> p_r_j = other->getPrevPos();
+   p_type p_r_ij_dist = p_r_i.dist(p_r_j);
+   Vector3D<p_type> p_r_ij = p_r_i - p_r_j;
 
-   float U_r_ij = K_div_2 * (r_ij_dist - r_o)*(r_ij_dist - r_o); 
-   float p_U_r_ij = K_div_2 * (p_r_ij_dist - r_o)*(p_r_ij_dist - r_o); 
+   p_type U_r_ij = K_div_2 * (r_ij_dist - r_o)*(r_ij_dist - r_o); 
+   p_type p_U_r_ij = K_div_2 * (p_r_ij_dist - r_o)*(p_r_ij_dist - r_o); 
 
-   Vector3D b_force;
+   Vector3D<p_type> b_force;
    if(r_ij_dist == p_r_ij_dist) {
-      b_force.set(0.0,0.0,0.0);
+      b_force.set(p_type(0.0),p_type(0.0),p_type(0.0));
    } else {
-      b_force = r_ij * (-1.0/r_ij_dist) * ((U_r_ij - p_U_r_ij)/(r_ij_dist - p_r_ij_dist));  
+      b_force = r_ij * (p_type(-1.0)/r_ij_dist) * ((U_r_ij - p_U_r_ij)/(r_ij_dist - p_r_ij_dist));  
    }
 
    // update the force values
@@ -138,15 +159,15 @@ void bondF(Particle *me, Particle *other) {
 }
 
 // returns a safe random position that will not break a polymer bond
-Vector3D safe_water_pos(std::vector<Particle *> polymer_beads, float unisize){
+Vector3D<p_type> safe_water_pos(std::vector<Particle<p_type> *> polymer_beads, float unisize){
     bool safe=false;
-    Vector3D w_pos;
+    Vector3D<p_type> w_pos;
     while(!safe) {
       safe = true;
-      w_pos = randPos(unisize);
+      w_pos = rand2DPos(unisize);
       for(auto i=polymer_beads.begin(); i!=polymer_beads.end(); ++i){
-          Particle* p = *i;
-          if(p->getPos().dist(w_pos) <= 0.5) {
+          Particle<p_type>* p = *i;
+          if(p->getPos().dist(w_pos) <= 0.75) {
              safe = false;
           }  
       }
@@ -160,29 +181,36 @@ int main() {
    const float unisize = UNISIZE_D;
 
    // mass of the particles
-   const float mass_w = 1.0;
-   const float mass_p = 1.0;
+   const p_type mass_w = 1.0;
+   const p_type mass_p = 1.0;
 
    const unsigned num_cubes = 10;
 
-   SimSystem universe(unisize, DELTA_T, R_C, num_cubes, 0);
+   SimSystem<p_type> universe(p_type(unisize), p_type(DELTA_T), p_type(R_C), num_cubes, 0);
    
-   std::vector<Particle *> polymer_beads;
+   std::vector<Particle<p_type> *> polymer_beads;
 
-// S = 0; B = 1; A = 2;
+   // add water
+   for(int w=0; w<500; w++){
+       // we want to add water, but not in between a polymer bond
+       Particle<p_type> *p = new Particle<p_type>(safe_water_pos(polymer_beads, unisize), 0, mass_w, conF, dragF, randF);
+       universe.addParticle(p);
+   }
+
+
    // add a polymer
    // create the polymer at a random position
-   for(int num_p=0; num_p <100; num_p++) {
-       Vector3D offset(0.0,0.5,0.0); //how far apart each bond particle is initially placed
-       Vector3D start = randPos(unisize);
-       Particle* p0 = new Particle(start, 1, mass_p, conF, dragF, randF); 
-       Particle* p1 = new Particle(p0->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF); 
-       Particle* p2 = new Particle(p1->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF); 
-       Particle* p3 = new Particle(p2->getPos().modulo_add(offset, unisize), 2, mass_p, conF, dragF, randF); 
-       Particle* p4 = new Particle(p3->getPos().modulo_add(offset, unisize), 2, mass_p, conF, dragF, randF); 
-       Particle* p5 = new Particle(p4->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF); 
-       Particle* p6 = new Particle(p5->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF); 
-       Particle* p7 = new Particle(p6->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF); 
+   for(int num_p=0; num_p <25; num_p++) {
+       Vector3D<p_type> offset(p_type(0.0),p_type(0.5),p_type(0.0)); //how far apart each bond particle is initially placed
+       Vector3D<p_type> start = rand2DPos(unisize);
+       Particle<p_type>* p0 = new Particle<p_type>(start, 1, mass_p, conF, dragF, randF);
+       Particle<p_type>* p1 = new Particle<p_type>(p0->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF);
+       Particle<p_type>* p2 = new Particle<p_type>(p1->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF);
+       Particle<p_type>* p3 = new Particle<p_type>(p2->getPos().modulo_add(offset, unisize), 2, mass_p, conF, dragF, randF);
+       Particle<p_type>* p4 = new Particle<p_type>(p3->getPos().modulo_add(offset, unisize), 2, mass_p, conF, dragF, randF);
+       Particle<p_type>* p5 = new Particle<p_type>(p4->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF);
+       Particle<p_type>* p6 = new Particle<p_type>(p5->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF);
+       Particle<p_type>* p7 = new Particle<p_type>(p6->getPos().modulo_add(offset, unisize), 1, mass_p, conF, dragF, randF);
 
        // keep a list of polymer beads to make sure we don't place a water bead between bonds
        polymer_beads.push_back(p0);
@@ -212,20 +240,11 @@ int main() {
        universe.bond(p4,p5, bondF);
        universe.bond(p5,p6, bondF);
        universe.bond(p6,p7, bondF);
+
    } 
 
-   // add water
-   for(int w=0; w<3000; w++){
-       // we want to add water, but not in between a polymer bond
-       Particle *p = new Particle(safe_water_pos(polymer_beads, unisize), 0, mass_w, conF, dragF, randF);
-       universe.addParticle(p);
-   }
-
-   // emit the initial state (read by the web renderer interface)
-   universe.emitJSONFromSU("state.json");
-
    // run the universe on a single thread for many timesteps, emitting it's value every 0.07 seconds 
-   universe.run(-1, 2.5);
+   universe.run(-1, 0.1);
 
    // emit the initial state (read by the web renderer interface)
    universe.emitJSONFromSU("state.json");
